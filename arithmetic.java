f9e71bcf2be7025325d8ad70d82070dc77b8068d
saivarshinithogiti <saivarshinithogiti@gmail.com>
2026-07-01 22:54:26 +0530

    operations

--- arithmetic.java
+++ arithmetic.java
@@ -1,15 +1,15 @@
 public class arithmetic {
-    public static int add(int a, int b) {
-        return a + b;
+    public static int add(int a,int b) {
+        return a+b;
     }
-    public static int sub(int a, int b) {
-        return a - b;
+    public static int sub(int a,int b) {
+        return a-b;
     }
-    public static int mult(int a, int b) {
-        return a * b;
+    public static int mult(int a,int b) {
+        return a*b;
     }
-    public static double div(double a, double b) {
-        return a / b;
+    public static double div(double a,double b) {
+        return a/b;
     }
     public static void main(String[] args) {
         int addition=add(10,20);
