70df8b70d0aade61bcf3aa1b198166a46cc59671
saivarshinithogiti <saivarshinithogiti@gmail.com>
2026-07-01 22:50:15 +0530

    if else statements

---
+++ condition.java
@@ -0,0 +1,9 @@
+public class condition {
+    public static void main(String[] args){
+        int marks = 60;
+        if(marks>=30)
+            System.out.println("Pass");
+        else
+            System.out.println("Fail");
+    }
+}
