887f05a6d934d89b1a69e5a6214e4d62a3192368
saivarshinithogiti <saivarshinithogiti@gmail.com>
2026-07-01 23:08:41 +0530

    changed code

--- arithmetic.java
+++ arithmetic.java
@@ -12,14 +12,10 @@ public class arithmetic {
         return a/b;
     }
     public static void main(String[] args) {
-        int addition=add(10,20);
-        int subtraction=sub(10, 20);
-        int multiplication=mult(10, 20);
-        double division=div(10, 20);
-
-        System.out.println("Addition: " + addition);
-        System.out.println("Subtraction: " + subtraction);
-        System.out.println("Multiplication: " + multiplication);
-        System.out.println("Division: " + division);
+        int a=10,b=20;
+        System.out.println(add(10,20));
+        System.out.println(sub(10,20));
+        System.out.println(mult(10,20));
+        System.out.println(div(10,20));
     }
 }
