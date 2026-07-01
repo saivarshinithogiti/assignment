5acc45517f0be987136606f59a7e4af78336c2f5
saivarshinithogiti <saivarshinithogiti@gmail.com>
2026-07-01 22:42:12 +0530

    operations

---
+++ arithmetic.java
@@ -0,0 +1,25 @@
+public class arithmetic {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+    public static int mult(int a, int b) {
+        return a * b;
+    }
+    public static double div(double a, double b) {
+        return a / b;
+    }
+    public static void main(String[] args) {
+        int addition=add(10,20);
+        int subtraction=sub(10, 20);
+        int multiplication=mult(10, 20);
+        double division=div(10, 20);
+
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
+    }
+}
